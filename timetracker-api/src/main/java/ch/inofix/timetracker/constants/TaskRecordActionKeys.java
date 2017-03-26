87e30f6b932f64af4c15e4b12d2842e392859cf8
Christian Berndt <berndt@inofix.ch>
2017-03-26 22:55:28 +0200

    Removed unnecessary action constants
    since they are already defined in the super class.

--- timetracker-api/src/main/java/ch/inofix/timetracker/constants/TaskRecordActionKeys.java
+++ timetracker-api/src/main/java/ch/inofix/timetracker/constants/TaskRecordActionKeys.java
@@ -6,8 +6,8 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
  *
  * @author Christian Berndt, Stefan Luebbers
  * @created 2017-03-21 13:41
- * @modified 2017-03-22 23:35
- * @version 1.0.1
+ * @modified 2017-03-26 22:34
+ * @version 1.0.2
  *
  */
 public class TaskRecordActionKeys extends ActionKeys {
@@ -15,10 +15,5 @@ public class TaskRecordActionKeys extends ActionKeys {
     public static final String ADD_TASK_RECORD = "ADD_TASK_RECORD";
     public static final String IMPORT_TASK_RECORDS = "IMPORT_TASK_RECORDS";
     public static final String DELETE_GROUP_TASK_RECORDS = "DELETE_GROUP_TASK_RECORDS";
-    public static final String VIEW = "VIEW";
-    public static final String PERMISSIONS = "PERMISSIONS";
-    public static final String DELETE = "DELETE";
-    public static final String UPDATE = "UPDATE";
-    
 
 }
