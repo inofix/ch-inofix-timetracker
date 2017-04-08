6925076e7a8a91b2cfb989b6443e31985a9e238b
Christian Berndt <berndt@inofix.ch>
2017-04-08 23:43:52 +0200

    New addTempFileEntry service method.

--- timetracker-service/src/main/java/ch/inofix/timetracker/service/impl/TaskRecordServiceImpl.java
+++ timetracker-service/src/main/java/ch/inofix/timetracker/service/impl/TaskRecordServiceImpl.java
@@ -14,15 +14,25 @@
 
 package ch.inofix.timetracker.service.impl;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Digester;
+import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
 
 import aQute.bnd.annotation.ProviderType;
 import ch.inofix.timetracker.constants.TaskRecordActionKeys;
@@ -47,8 +57,8 @@ import ch.inofix.timetracker.service.permission.TimetrackerPortletPermission;
  *
  * @author Christian Berndt, Stefan Luebbers
  * @created 2015-05-07 23:50
- * @modified 2017-03-25 19:41
- * @version 1.0.9
+ * @modified 2017-04-08 19:29
+ * @version 1.1.0
  * @see TaskRecordServiceBaseImpl
  * @see ch.inofix.timetracker.service.TaskRecordServiceUtil
  */
@@ -71,6 +81,20 @@ public class TaskRecordServiceImpl extends TaskRecordServiceBaseImpl {
         return taskRecordLocalService.addTaskRecord(getUserId(), workPackage, description, ticketURL, endDate,
                 startDate, status, duration, serviceContext);
 
+    }
+
+    @Override
+    public FileEntry addTempFileEntry(long groupId, String folderName, String fileName, InputStream inputStream,
+            String mimeType) throws PortalException {
+
+        _log.info("addTempFileEntry()");
+
+        // TODO
+        // GroupPermissionUtil.check(getPermissionChecker(), groupId,
+        // TaskRecordActionKeys.IMPORT_TASK_RECORDS);
+
+        return TempFileEntryUtil.addTempFileEntry(groupId, getUserId(),
+                DigesterUtil.digestHex(Digester.SHA_256, folderName), fileName, inputStream, mimeType);
     }
 
     @Override
@@ -121,8 +145,44 @@ public class TaskRecordServiceImpl extends TaskRecordServiceBaseImpl {
     }
 
     @Override
+    public long importTaskRecordsInBackground(ExportImportConfiguration taskRecordConfiguration, File file)
+            throws PortalException {
+
+        _log.info("importTaskRecordsInBackground()");
+
+        Map<String, Serializable> settingsMap = taskRecordConfiguration.getSettingsMap();
+
+        long targetGroupId = MapUtil.getLong(settingsMap, "targetGroupId");
+
+        // TODO
+        // GroupPermissionUtil.check(
+        // getPermissionChecker(), targetGroupId,
+        // TaskRecordActionKeys.IMPORT_TASK_RECORDS);
+
+        return taskRecordLocalService.importTaskRecordsInBackground(getUserId(), taskRecordConfiguration, file);
+    }
+
+    @Override
+    public long importTaskRecordsInBackground(ExportImportConfiguration taskRecordConfiguration,
+            InputStream inputStream) throws PortalException {
+
+        _log.info("importTaskRecordsInBackground()");
+
+        Map<String, Serializable> settingsMap = taskRecordConfiguration.getSettingsMap();
+
+        long targetGroupId = MapUtil.getLong(settingsMap, "targetGroupId");
+
+        // TODO
+        // GroupPermissionUtil.check(
+        // getPermissionChecker(), targetGroupId,
+        // TaskRecordActionKeys.IMPORT_TASK_RECORDS);
+
+        return taskRecordLocalService.importTaskRecordsInBackground(getUserId(), taskRecordConfiguration, inputStream);
+    }
+
+    @Override
     public Hits search(long userId, long groupId, String keywords, int start, int end, Sort sort)
-            throws PortalException{
+            throws PortalException {
 
         return taskRecordLocalService.search(userId, groupId, keywords, start, end, sort);
     }
