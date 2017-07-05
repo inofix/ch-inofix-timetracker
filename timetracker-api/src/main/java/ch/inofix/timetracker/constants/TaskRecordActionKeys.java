efb9b1e4bceaea4ce983333c813da60c323d91a4
Christian Berndt <berndt@inofix.ch>
2017-07-05 13:17:15 +0200

    Consolidated task-record permissions.

--- timetracker-api/src/main/java/ch/inofix/timetracker/constants/TaskRecordActionKeys.java
+++ timetracker-api/src/main/java/ch/inofix/timetracker/constants/TaskRecordActionKeys.java
@@ -7,16 +7,16 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
  * @author Christian Berndt
  * @author Stefan Luebbers
  * @created 2017-03-21 13:41
- * @modified 2017-06-20 18:21
- * @version 1.0.4
+ * @modified 2017-07-05 13:16
+ * @version 1.0.5
  *
  */
 public class TaskRecordActionKeys extends ActionKeys {
 
     public static final String ADD_TASK_RECORD = "ADD_TASK_RECORD";
+    public static final String DELETE_GROUP_TASK_RECORDS = "DELETE_GROUP_TASK_RECORDS";
     public static final String EXPORT_TASK_RECORDS = "EXPORT_TASK_RECORDS";
     public static final String EXPORT_IMPORT_TASK_RECORDS = "EXPORT_TASK_RECORDS";
     public static final String IMPORT_TASK_RECORDS = "IMPORT_TASK_RECORDS";
-    public static final String DELETE_GROUP_TASK_RECORDS = "DELETE_GROUP_TASK_RECORDS";
 
 }
