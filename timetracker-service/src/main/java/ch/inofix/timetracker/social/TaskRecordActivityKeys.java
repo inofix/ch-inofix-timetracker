87528ebec0572b596bf4d9de79aef5973a2c5dac
Christian Berndt <berndt@inofix.ch>
2017-07-09 18:04:39 +0200

    Social activities for timetracker.

---
+++ timetracker-service/src/main/java/ch/inofix/timetracker/social/TaskRecordActivityKeys.java
@@ -0,0 +1,17 @@
+package ch.inofix.timetracker.social;
+
+/**
+ *
+ * @author Christian Berndt
+ * @created 2017-07-09 17:10
+ * @modified 2017-07-09 17:10
+ * @version 1.0.0
+ *
+ */
+public class TaskRecordActivityKeys {
+
+    public static final int ADD_TASK_RECORD = 1;
+
+    public static final int UPDATE_TASK_RECORD = 2;
+
+}
