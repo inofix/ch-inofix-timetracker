0f5d06c6d4fdcba368821e281eb8932ee5d3f7ec
Christian Berndt <berndt@inofix.ch>
2017-08-22 22:09:00 +0200

    Consolidated local-service test setup.

---
+++ timetracker-test/src/testIntegration/java/ch/inofix/timetracker/service/test/TaskRecordLocalServiceTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package ch.inofix.timetracker.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import ch.inofix.timetracker.model.TaskRecord;
+import ch.inofix.timetracker.service.TaskRecordLocalServiceUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Christian Berndt
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class TaskRecordLocalServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule = new AggregateTestRule(new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		_group = GroupTestUtil.addGroup();
+		_user = TestPropsValues.getUser();
+
+	}
+
+	@Test
+	public void testCreateTaskRecord() throws Exception {
+
+		TaskRecord taskRecord = TaskRecordLocalServiceUtil.createTaskRecord(0);
+
+		Assert.assertNotNull(taskRecord);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(TaskRecordLocalServiceTest.class.getName());
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	private User _user;
+
+}
