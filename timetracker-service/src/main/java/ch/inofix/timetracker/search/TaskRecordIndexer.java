1f366e7c48b3b77f5a6da3d3e57cc2916c8653b2
Christian Berndt <berndt@inofix.ch>
2017-09-20 12:20:16 +0200

    Re-enabled PrefixFilter.

--- timetracker-service/src/main/java/ch/inofix/timetracker/search/TaskRecordIndexer.java
+++ timetracker-service/src/main/java/ch/inofix/timetracker/search/TaskRecordIndexer.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.BaseIndexer;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
@@ -27,6 +28,7 @@ import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.PrefixFilter;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -42,8 +44,8 @@ import ch.inofix.timetracker.service.permission.TaskRecordPermission;
  * @author Christian Berndt
  * @author Stefan Luebbers
  * @created 2016-11-26 15:04
- * @modified 2017-09-19 21:09
- * @version 1.1.2
+ * @modified 2017-09-20 12:16
+ * @version 1.1.3
  *
  */
 @Component(immediate = true, service = Indexer.class)
@@ -94,6 +96,23 @@ public class TaskRecordIndexer extends BaseIndexer<TaskRecord> {
         }
 
         contextBooleanFilter.addRangeTerm("fromDate_Number_sortable", min, max);
+        
+        // workPackage
+
+        String workPackage = (String) searchContext.getAttribute("workPackage");
+
+        if (Validator.isNotNull(workPackage)) {
+
+            BooleanFilter booleanFilter = new BooleanFilter();
+
+            // Use the sortable index field, since the regular field is
+            // analyzed, which means, we can't use hyphens in workpackage
+            // names.
+            PrefixFilter prefixFilter = new PrefixFilter("workPackage_sortable", workPackage);
+
+            booleanFilter.add(prefixFilter);
+            contextBooleanFilter.add(booleanFilter, BooleanClauseOccur.MUST);
+        }
 
     }
 
@@ -181,17 +200,17 @@ public class TaskRecordIndexer extends BaseIndexer<TaskRecord> {
                 isCommitImmediately());
     }
     
-    @Override 
-    protected void postProcessFullQuery(BooleanQuery fullQuery, SearchContext searchContext) {
-
-        String workPackage = (String) searchContext.getAttribute("workPackage");
-
-        if (Validator.isNotNull(workPackage)) {       
-            
-            fullQuery.addRequiredTerm("workPackage_sortable", workPackage);
-        }
-        
-    }
+//    @Override 
+//    protected void postProcessFullQuery(BooleanQuery fullQuery, SearchContext searchContext) {
+//
+//        String workPackage = (String) searchContext.getAttribute("workPackage");
+//
+//        if (Validator.isNotNull(workPackage)) {       
+//            
+//            fullQuery.addRequiredTerm("workPackage_sortable", workPackage);
+//        }
+//        
+//    }
 
     protected void reindexTaskRecords(long companyId) throws PortalException {
 
